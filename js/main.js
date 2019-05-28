19f0f016dddeffc1363ad38716896015bc417373
Praneet9 <praneetbomma@gmail.com>
2019-05-29 00:46:42 +0530

    basic input output of terminal done

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
 // custom js
+$(function () {
+    $('#input_command').keypress(function (e) {
+        // Enter pressed?
+        if (e.which == 10 || e.which == 13) {
+            var test = $('#input_command').val()
+            $('#input_command').val("")
+            $( "#terminal_op" ).append( "<p>"+test+"</p>" );
+            console.log(test);
+            
+        }
+    });
+
+});
