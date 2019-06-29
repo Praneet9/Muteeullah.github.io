03c89f99ef6dc20232ac77ee017df46b850f58c2
Felix <md@Felixs-Mac-mini.local>
2019-06-29 16:57:39 +0530

    added few details

--- js/main.js
+++ js/main.js
@@ -8,31 +8,31 @@ $('#input_command').keypress(function (e) {
         $('#input_command').val("");
         $("#terminal_operations").append("<p>$ "+ command + "</p>");
         switch (command){
-            case 'help':
+            case 'cat help':
                     $("#terminal_operations").append("<p class='text-info'>Available commands</p>");
-                $("#terminal_operations").append("<p>about : [Know about me]</p>");
-                $("#terminal_operations").append("<p>skills : [Get to know what I'm skilled yet]</p>");
-                $("#terminal_operations").append("<p>activities : [Know more about activities I've been a part of]</p>");    
-                $("#terminal_operations").append("<p>projects : [The projects I've worked on]</p>");
+                $("#terminal_operations").append("<p>cat about : [Know about me]</p>");
+                $("#terminal_operations").append("<p>cat skills : [Get to know what I'm skilled yet]</p>");
+                $("#terminal_operations").append("<p>cat activities : [Know more about activities I've been a part of]</p>");    
+                $("#terminal_operations").append("<p>cat projects : [The projects I've worked on]</p>");
                 $("#terminal_operations").append("<p>resume : [Download my resume]</p>");
                 break;
-            case 'about':
+            case 'cat about':
+                $("#terminal_operations").append("<p class='text-info'>I am a Computer Science graduate, started learning as a Java Web Application Developer got interested in Managing and Deploying Application on Servers and finally paving the way to develop the interest towards the field of CyberSecurity. </p>");
+                break;
+            case 'cat activities':
+                $("#terminal_operations").append("<p class='text-info'>  I was one of the head of THE PROGRAMMERS CLUB, a community that helps other students to code. I was also a speaker at a workshop on Object Oriented Programming (JAVA) organized by the club and also a part of the Core & Technical committee that organized ERR_404 State Level Hackathon. Mostly we (me and my team) occasionally took part in Hackathons. </p>");
+                break;
+            case 'cat resume':
                 $("#terminal_operations").append("<p class='text-info'>Under development, will be updated soon!</p>");
                 break;
-            case 'activities':
-                $("#terminal_operations").append("<p class='text-info'>Under development, will be updated soon!</p>");
+            case 'cat skills':
+                $("#terminal_operations").append("<p class='text-info'>PENTESTING | SYSTEM ADMINISTRATION | WEB DEVELOPMENT</p>");
                 break;
-            case 'resume':
-                $("#terminal_operations").append("<p class='text-info'>Under development, will be updated soon!</p>");
-                break;
-            case 'skills':
-                $("#terminal_operations").append("<p class='text-info'>Under development, will be updated soon!</p>");
-                break;
-            case 'projects':
+            case 'cat projects':
                 $("#terminal_operations").append("<p class='text-info'>Under development, will be updated soon!</p>");
                 break;
             default:
-                $("#terminal_operations").append("<p class='text-warning'>command not found: " + command + "</p>");
+                $("#terminal_operations").append("<p class='text-warning'>You do not have permission: " + command + "</p>");
         }
     }
 });
